9026b8641a2da42741ebbefdcf748a49c6335f35
Nory Lazo <nlazo@icloud.com>
2019-11-12 03:41:22 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(collection)
   i = 0 
   
+  collect = []
   while i < collection.length
-    yield(collection[i])
+    yield( collect << collection[i].split(" ").first )
   
     i += 1
   end
